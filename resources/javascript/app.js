ae16c06f6b4eaff92e1bf861ed8d4263c95a6ec6
Yaroslav Hyrych <yaroslav.hyrych@gmail.com>
2016-03-05 17:04:33 +0200

    Add media

--- resources/javascript/app.js
+++ resources/javascript/app.js
@@ -1,18 +1,51 @@
-$(window).scroll(function() {
+(function($) {
+    var $window = $(window);
+    var $document = $(document);
 
-    var wScroll = $(this).scrollTop();
+    function rebuildFrontPage() {
+        var width = $window.width();
+        var height = $window.height();
+        height -= height /5;
 
-    var pcPosY = wScroll /2;
-    $('.pc').css({
-        'transform' : 'translate(0px, -'+ pcPosY + 'px)'
+        $('#front-page').css({
+            height: height
+        });
+    }
+
+    $window.scroll(function() {
+
+        var wScroll = $(this).scrollTop();
+
+        var pcPosY = wScroll /2;
+        $('#pc').css({
+            'transform' : 'translate(0px, -'+ pcPosY + 'px)'
+        });
+
+        var phonePosY = wScroll /4;
+        $('.phone').css({
+            'transform' : 'translate(0px, -'+ phonePosY + 'px)'
+        });
+
+        //$('.projects > header').css({
+        //    'transform' : 'translate(-' +  wScroll /2 + 'px, 0px)'
+        //});
     });
 
-    var phonePosY = wScroll /4;
-    $('.phone').css({
-        'transform' : 'translate(0px, -'+ phonePosY + 'px)'
-    });
+    $window.on('resize orientationchange', rebuildFrontPage);
 
-    //$('.projects > header').css({
-    //    'transform' : 'translate(-' +  wScroll /2 + 'px, 0px)'
-    //});
-});
+    $document.ready(function() {
+        rebuildFrontPage();
+
+        $('div[class*=contact]').hover(function() {
+            //console.log($())
+        });
+
+    });
+})(jQuery);
+
+
+
+
+
+
+
